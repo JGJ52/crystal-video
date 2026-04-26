8ad49da5fd7c8d9941b8a44387599b20a77262a9
jgj52 <email@jgj52.hu>
2026-04-26 13:52:44 +0200

    why didnt this get added

---
+++ app/videos/icon-not-page.js
@@ -0,0 +1,13 @@
+import "./icon.css";
+
+export default function IconNotPage({ children }) {
+    return (
+        <>
+            <div className={"centerer"}>
+                <div className={"icon"}>
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
