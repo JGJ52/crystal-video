c65f950be239d25f714277bcd9c43fed35e007ec
jgj52 <email@jgj52.hu>
2026-04-26 13:06:45 +0200

    i made video player!!

--- app/videos/videos.js
+++ app/videos/videos.js
@@ -1,9 +1,14 @@
 "use client"
 import "./videos.css";
-import { useEffect, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import Video from "@/app/videos/video";
 import Navbar from "@/app/navbar/navbar";
 import {videos} from "@/instrumentation";
+import Button from "@/app/navbar/button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faExpand, faPause, faPlay, faVolumeHigh, faVolumeMute} from "@fortawesome/free-solid-svg-icons";
+import MaterialIcon from "@/app/material";
+import Icon from "@/app/videos/icon";
 
 export default function Videos({ uploader, setUploader }) {
     const [vids, setVideos] = useState([]);
@@ -19,12 +24,107 @@ export default function Videos({ uploader, setUploader }) {
     }, []);
 
     const [playing, setPlaying] = useState(null);
+    const ref = useRef(null);
+    const [play, setPlay] = useState(faPlay);
+    const timeoutRef = useRef(null);
+    const [showIcon, setShowIcon] = useState(false);
+    const time = useRef(null);
+    const [where, setWhere] = useState(0);
+
+    function playPause() {
+        const { current } = ref;
+        clearTimeout(timeoutRef.current);
+
+        if (!current.paused) {
+            current.pause();
+            setPlay(faPlay);
+        } else {
+            current.play();
+            setPlay(faPause);
+        }
+
+        setShowIcon(false);
+        setTimeout(() => {
+            setShowIcon(true);
+            timeoutRef.current = setTimeout(() => {
+                setShowIcon(false);
+            }, 1000);
+        }, 0);
+    }
+
+    function skip(time) {
+        ref.current.currentTime += time;
+    }
+
+    const [volume, setVolume] = useState(1);
+    const [muted, setMuted] = useState(false);
+
+    function mute() {
+        setMuted(!muted);
+        const { current } = ref;
+        current.volume = !muted ? 0 : volume;
+    }
+
+    function vol(e) {
+        const { current } = ref;
+        const volume = e.target.value / 100;
+        setVolume(volume);
+        if (!muted) {
+            current.volume = volume;
+        }
+    }
+
+    function timeUpdate() {
+        const { current } = ref;
+        const percent = (current.currentTime / current.duration) * 100;
+        setWhere(percent);
+        time.current.value = percent;
+    }
+
+    function otherTimeUpdate(current) {
+        const w = current.target.value;
+        if (w === where) return;
+        const { current: video } = ref;
+        video.currentTime = (w / 100) * video.duration;
+    }
 
     if (playing) {
         return (
             <>
                 <Navbar updateVideos={loadVideos} homeClick={() => setUploader(null)} />
-                <video src={`/videos/video/${playing.id}`} controls />
+                <div className="vid-ct">
+                    <video src={`/videos/video/${playing.id}`} ref={ref} onClick={playPause} onTimeUpdate={timeUpdate} />
+                    <input type={"range"} className={"volume time"} ref={time} onChange={e => otherTimeUpdate(e)} />
+                    <div className="controls">
+                        <div className="flex flex-row">
+                            <input type={"range"} onChange={e => vol(e)} className={"volume"} />
+                            <Button onClick={mute}>
+                                <FontAwesomeIcon icon={muted ? faVolumeMute : faVolumeHigh} />
+                            </Button>
+                        </div>
+                        <div className="flex flex-row">
+                            <Button onClick={() => skip(-10)}>
+                                <MaterialIcon icon={"fast_rewind"} />
+                            </Button>
+                            <Button onClick={playPause}>
+                                <FontAwesomeIcon icon={play} />
+                            </Button>
+                            <Button onClick={() => skip(10)}>
+                                <MaterialIcon icon={"fast_forward"} />
+                            </Button>
+                        </div>
+                        <div className="flex flex-row relative">
+                            <Button onClick={async () => await ref.current.requestFullscreen()}>
+                                <FontAwesomeIcon icon={faExpand} />
+                            </Button>
+                        </div>
+                    </div>
+                    {showIcon && (
+                        <Icon>
+                            <FontAwesomeIcon icon={play} size="3x" />
+                        </Icon>
+                    )}
+                </div>
             </>
         )
     }
